9aa463b1af2b03313ba2f48eb026c9a9bed73d9a
alfredo-allan <allan-mecanico@outlook.com>
2025-03-23 00:21:02 -0300

    ajustando para a vercel

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../Pages/HomePage/HomePage';
-import AboutPage from '../Pages/AboutPage/AboutPage'; // Importe o AboutPage
+import AboutPage from '../Pages/AboutPage/AboutPage';
 
-const RoutesComponent = () => {
+interface RoutesComponentProps { }
+
+const RoutesComponent: React.FC<RoutesComponentProps> = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />} /> {/* Mudou de /leap-in-technology para / */}
+                <Route path="/" element={<HomePage />} />
                 <Route path="about" element={<AboutPage />} />
             </Routes>
         </BrowserRouter>
