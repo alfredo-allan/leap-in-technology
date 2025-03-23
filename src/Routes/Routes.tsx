91895fdfa96fb0297665a05d8ee89f67b95b534e
alfredo-allan <allan-mecanico@outlook.com>
2025-03-22 21:22:53 -0300

    corrigindo bloqueio de img git

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -8,7 +8,7 @@ const RoutesComponent = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/leap-in-technology" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
+                <Route path="about" element={<AboutPage />} />
             </Routes>
         </BrowserRouter>
     );
